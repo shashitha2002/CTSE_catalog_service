0c38c3072293101161014c12002ff64724d7f657
shashitha2002 <bimsarashashitha@gmail.com>
2026-03-21 14:33:42 +0530

    added a new api function taken from the user service

--- src/services/userServiceClient.ts
+++ src/services/userServiceClient.ts
@@ -1,15 +1,22 @@
 import axios from "axios";
 import 'dotenv/config'
+import { token } from "morgan";
 
-const USER_SERVICE_URL = process.env.USER_SERVICE_URL;
+const API_GATEWAY_URL = process.env.API_GATEWAY_URL || "http://localhost:8080";
 
 export interface VerifyUserResponse {
     "isAdmin": boolean
 }
+
+export interface UserRoleCheckResponse {
+    "isAdmin": false,
+    "role": string
+}
+
 export const verifyUser = async (userId: string): Promise<VerifyUserResponse> => {
     try {
         const response = await axios.get<VerifyUserResponse>(
-            `${USER_SERVICE_URL}/users/${userId}/verify`
+            `${API_GATEWAY_URL}/users/${userId}/verify`
         );
         console.log(response.data);
         return response.data;
@@ -21,6 +28,25 @@ export const verifyUser = async (userId: string): Promise<VerifyUserResponse> =>
         throw new Error(`User Service unavailable: ${error.message}`);
     }
 };
+
+export const  userRoleCheck = async (token: string): Promise<UserRoleCheckResponse> => {
+    try{
+        const response = await axios.get<UserRoleCheckResponse>(`${API_GATEWAY_URL}/users/me/admin`, {
+            headers: {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log("admin data",response.data);
+        return response.data;
+
+    }catch(error: any){
+        if (error.response?.status === 404) {
+            return {"isAdmin": false, "role": "user"};
+        }
+        throw new Error(`User Service unavailable: ${error.message}`);
+    }
+}
 
 /**
  * Returns true only if the user exists AND has role 'admin'
