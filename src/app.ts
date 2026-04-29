55695d898b3a355be1a544164055cd32a26988e0
shashitha2002 <bimsarashashitha@gmail.com>
2026-04-29 18:22:26 +0530

    removed unwanted comment to check the pipeline

--- src/app.ts
+++ src/app.ts
@@ -9,12 +9,11 @@ import { swaggerSpec } from './config/swagger.js';
 const app: Application = express();
 
 // Middleware
-app.use(helmet()); // Adds security headers
+app.use(helmet());
 app.use(cors());
-app.use(morgan('dev')); // Logs requests to the console
+app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-// Health Check (For Cloud Providers like Render to know the service is alive)
 app.get('/health', (req: Request, res: Response) => {
     res.status(200).json({ service: 'Book Catalog Service', status: 'UP' });
 });
